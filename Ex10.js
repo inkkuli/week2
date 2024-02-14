18f6bf372d45a2d843ea57fdc7ccccd7de8a4637
inkkuli <124854510+inkkuli@users.noreply.github.com>
2024-02-14 16:20:18 +0700

    Add files via upload

---
+++ Ex10.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Ex08() {
+  return (
+    <View style={{ flex : 1,backgroundColor : '#4A90E2' }}>  
+
+
+    </View>
+  );
+}
